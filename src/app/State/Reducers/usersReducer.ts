f4b41aa3310959b9c79fbb50b7e35c978fab43e5
Milgo21 <felixmilgo21@gmail.com>
2023-03-05 00:03:57 +0300

    state and update

--- src/app/State/Reducers/usersReducer.ts
+++ src/app/State/Reducers/usersReducer.ts
@@ -1,19 +1,40 @@
+import { createReducer, on, createFeatureSelector } from "@ngrx/store";
 import { User } from "src/app/Interfaces";
+import { loginFail,loginUserSuccess, loginUser } from "../Actions/userActions";
 
 
 
 export interface UserInterface{
   user:any
   error:string
+  loginUser:string
   loginSuccess:string
   loginFail:string
 }
 const initialState:UserInterface = {
-  user:"",
-  error:"",
-  loginSuccess:"",
-  loginFail:""
+  user: null,
+  error: "",
+  loginSuccess: "",
+  loginFail: "",
+  loginUser: ""
 }
 
+const userSliceState= createFeatureSelector<UserInterface>('user')
+export const userReducer=createReducer<UserInterface>(
+  initialState,
+  on(loginUserSuccess, (state,actions):UserInterface=>{
+     return {
+      ...state,
+      loginFail:'',
+      loginSuccess:actions.type
+     }
+  })
+  ,  on(loginFail, (state,actions):UserInterface=>{
+      return {
+      ...state,
+      error:actions.error,
+      loginSuccess:""
+      }
+   })
 
-let a:number = 2;
+)
