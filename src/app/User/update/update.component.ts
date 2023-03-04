a154df4e79a3b8df3bd02a5643688ead7ac94962
Milgo21 <felixmilgo21@gmail.com>
2023-03-05 00:30:19 +0300

    update form

--- src/app/User/update/update.component.ts
+++ src/app/User/update/update.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-update',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './update.component.html',
   styleUrls: ['./update.component.css']
 })
-export class UpdateComponent {
+export class UpdateComponent implements OnInit{
+  updateForm!:FormGroup
+  constructor(){}
+  ngOnInit():void{
+    this.updateForm = new FormGroup({
+      name:new FormControl(null,[Validators.required]),
+      email:new FormControl(null,[Validators.required, Validators.email]),
+      password:new FormControl(null,[Validators.required])
+    })
+  }
+
+
+  updateProfile(){
+
+  }
 
 }
